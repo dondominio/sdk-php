484ff8b6f1141829584a4cbee2cc905a537569e6
Miky <miky@kuro-neko.es>
2016-05-04 19:06:45 +0200

    Update config-check.php

--- config-check.php
+++ config-check.php
@@ -9,7 +9,7 @@
 define( 'YOUR_API_USER', '' );
 define( 'YOUR_API_PASSWORD', '' );
 
-init_set( 'display_errors', '1' );
+ini_set( 'display_errors', '1' );
 error_reporting( E_ALL );
 
 require_once( 'src/DonDominioAPI.php' );
