75d5983a50e2da2ccaf90858c84a92a5d098dc35
playmono <adrgranado@gmail.com>
2020-10-21 16:30:08 +0200

    autoloader identation

--- src/autoloader.php
+++ src/autoloader.php
@@ -7,12 +7,12 @@
 spl_autoload_register(function($class) {
     $prefix = 'Dondominio\API';
     $prefixLength = strlen($prefix);
-		$basePath = __DIR__;
-		if (0 === strncmp($prefix, $class, $prefixLength)) {
-			$file = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $prefixLength));
-			$file = realpath( $basePath . (empty($file) ? '' : DIRECTORY_SEPARATOR) . $file . '.php');
-			if( @file_exists( $file )) {
-				require_once( $file );
-			}
-		}
+    $basePath = __DIR__;
+    if (0 === strncmp($prefix, $class, $prefixLength)) {
+        $file = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $prefixLength));
+        $file = realpath($basePath . (empty($file) ? '' : DIRECTORY_SEPARATOR) . $file . '.php');
+        if( @file_exists($file)) {
+            require_once($file);
+        }
+    }
 });
