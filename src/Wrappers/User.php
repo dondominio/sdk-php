010bbdba1675728bc78c5e2c181818d33797f389
JAndreuSoler <joanandreusole@gmail.com>
2021-06-29 09:27:38 +0200

    New action to add domain to user in User ( user_addDomain() )

--- src/Wrappers/User.php
+++ src/Wrappers/User.php
@@ -152,4 +152,61 @@ class User extends \Dondominio\API\Wrappers\AbstractWrapper
         return $this->execute('user/delete/', $args, $map);
     }
 
+    /**
+     * Add domain to User
+     *
+     * * ! = required
+     * ! domainName             string		Domain Name
+     * - domain_ownercontacts   string
+     * - domain_contacts        string
+     * - domain_dns             string
+     * - domain_gluerecords     string
+     * - domain_dnssec          string
+     * - domain_transferblock   string
+     * - domain_authcode        string
+     * - domain_others          string
+     * - service_ftp            string
+     * - service_subdomains     string
+     * - service_redirects      string
+     * - service_parking        string
+     * - service_dnszone        string
+     * - service_ddbb           string
+     * - servicemail_accounts   string
+     * - servicemail_alias      string
+     * 
+     * @link https://dev.dondominio.com/api/docs/api/
+     *
+     * @param string $username User Username
+     * @param array $args Domain Name
+     *
+     * @return	\Dondominio\API\Response\Response
+     */
+    protected function addDomain(string $username, array $args = [])
+    {
+        $args['username'] = $username;
+
+        $map = [
+            ['name' => 'username',              'type' => 'string',     'required' => true],
+            ['name' => 'domainName',            'type' => 'string',     'required' => true],
+            ['name' => 'domain_ownercontacts',  'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'domain_contacts',       'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'domain_dns',            'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'domain_gluerecords',    'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'domain_dnssec',         'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'domain_transferblock',  'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'domain_authcode',       'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'domain_others',         'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'service_ftp',           'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'service_subdomains',    'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'service_redirects',     'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'service_parking',       'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'service_dnszone',       'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'service_ddbb',          'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'servicemail_accounts',  'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+            ['name' => 'servicemail_alias',     'type' => 'list',       'required' => false,    'list' => ['write', 'read', 'disabled']],
+        ];
+
+        return $this->execute('user/adddomain/', $args, $map);
+    }
+
 }
